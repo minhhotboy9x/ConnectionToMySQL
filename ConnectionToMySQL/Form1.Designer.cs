e0db2dcf1026ad38b351b148fa5ca3bf3dd13ace
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2023-01-29 01:32:48 +0700

    user control

--- ConnectionToMySQL/Form1.Designer.cs
+++ ConnectionToMySQL/Form1.Designer.cs
@@ -32,13 +32,14 @@ namespace ConnectionToMySQL
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.userControl11 = new ConnectionToMySQL.UserControl1();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.button1.Location = new System.Drawing.Point(266, 394);
+            this.button1.Location = new System.Drawing.Point(154, 345);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(105, 43);
             this.button1.TabIndex = 0;
@@ -62,23 +63,32 @@ namespace ConnectionToMySQL
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(654, 360);
+            this.dataGridView1.Size = new System.Drawing.Size(505, 282);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(506, 394);
+            this.textBox1.Location = new System.Drawing.Point(375, 345);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(194, 22);
             this.textBox1.TabIndex = 2;
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.Color.Tan;
+            this.userControl11.Location = new System.Drawing.Point(599, 12);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(229, 225);
+            this.userControl11.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(881, 450);
+            this.Controls.Add(this.userControl11);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
@@ -95,6 +105,7 @@ namespace ConnectionToMySQL
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox1;
+        private UserControl1 userControl11;
     }
 }
 
